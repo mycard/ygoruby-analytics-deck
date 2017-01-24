9269be482e431703ae502f9e565bfaab77736b30
Peter Xin <xinguangyao@gmail.com>
2017-01-24 14:53:07 +0800

    Fix

--- API.rb
+++ API.rb
@@ -4,7 +4,7 @@ require File.dirname(__FILE__) + '/../../YgorubyBase/Deck.rb'
 require File.dirname(__FILE__) + '/Compiler.rb'
 require File.dirname(__FILE__) + '/GitSyncer.rb'
 
-Plugin.api.push 'get', '/deckIdentifier/json' do
+Plugin.api.push 'get', '/analyze/deckIdentifier/json' do
 	json    = request.body.read
 	deck    = Deck.from_hash json
 	content = DeckIdentifier.global[deck]
@@ -18,7 +18,7 @@ end
 ## 测试环境
 test_environment = DeckIdentifier.new 'test'
 
-Plugin.api.push 'post', '/deckIdentifier/test/compiler' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/test/compiler' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, ' compiled a string file.'
 	def_str = request.body.read
 	def_str.force_encoding 'utf-8'
@@ -27,7 +27,7 @@ Plugin.api.push 'post', '/deckIdentifier/test/compiler' do
 	content.to_json
 end
 
-Plugin.api.push 'post', '/deckIdentifier/test/compiler/heavy' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/test/compiler/heavy' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, ' compiled a string file and classificationized it.'
 	def_str = request.body.read
 	def_str.force_encoding 'utf-8'
@@ -37,7 +37,7 @@ Plugin.api.push 'post', '/deckIdentifier/test/compiler/heavy' do
 	content.to_json
 end
 
-Plugin.api.push 'post', '/deckIdentifier/test' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/test' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, ' posted a string file into the test environment.'
 	json    = request.body.read
 	content = JSON.parse json
@@ -51,7 +51,7 @@ Plugin.api.push 'delete', '/deckIdentifier/test' do
 	'cleared'
 end
 
-Plugin.api.push 'post', '/deckIdentifier/test/test' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/test/test' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, ' request to test a deck.'
 	content = request.body.read
 	deck    = Deck.load_ydk_str content
@@ -60,14 +60,14 @@ Plugin.api.push 'post', '/deckIdentifier/test/test' do
 	content.to_json
 end
 
-Plugin.api.push 'post', '/deckIdentifier/test/load' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/test/load' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, ' requested to import production into test environment.'
 	test_environment.register_config
 	test_environment.finish
 	'loaded'
 end
 
-Plugin.api.push 'post', '/deckIdentifier/test/reset' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/test/reset' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, 'reset the test environment.'
 	test_environment.clear
 	test_environment.register_config
@@ -77,18 +77,18 @@ end
 
 
 ## 工作环境
-Plugin.api.push 'get', '/deckIdentifier/product/list' do
+Plugin.api.push 'get', '/analyze/deckIdentifier/product/list' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, 'requested the file list.'
 	content_type 'application/json'
 	DeckIdentifier.get_config_file_list.to_json
 end
 
-Plugin.api.push 'get', '/deckIdentifier/product' do
+Plugin.api.push 'get', '/analyze/deckIdentifier/product' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, 'requested the file ' + params['filename']
 	DeckIdentifier.get_config_file params['filename'] + '.deckdef'
 end
 
-Plugin.api.push 'post', '/deckIdentifier/product' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/product' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, 'posted the file ' + params['filename']
 	DeckIdentifier.save_config_file_to params['filename'] + '.deckdef', params['file']
 	'posted'
@@ -100,7 +100,7 @@ Plugin.api.push 'delete', '/deckIdentifier/product' do
 	'removed'
 end
 
-Plugin.api.push 'post', '/deckIdentifier/restart' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/restart' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, 'restarted the production server config.'
 	DeckIdentifier.global.clear
 	DeckIdentifier.global.register_config
@@ -108,18 +108,18 @@ Plugin.api.push 'post', '/deckIdentifier/restart' do
 end
 
 ## Git 操作
-Plugin.api.push 'post', '/deckIdentifier/git/pull' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/git/pull' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, 'pulled from git.'
 	GitSyncer.pull
 	'fin'
 end
 
-Plugin.api.push 'post', '/deckIdentifier/git/push' do
+Plugin.api.push 'post', '/analyze/deckIdentifier/git/push' do
 	DeckIdentifier.quick_access_key params['accesskey'], self, 'pushed to the git.'
 	GitSyncer.push
 	'fin'
 end
 
 # 静态页面
-Plugin.api.push 'static', { ['/deckIdentifier/console/'] => 'Plugins/DeckIdentifier/Server/'} do
+Plugin.api.push 'static', { ['/deckIdentifier/'] => 'Plugins/DeckIdentifier/Server/'} do
 end
